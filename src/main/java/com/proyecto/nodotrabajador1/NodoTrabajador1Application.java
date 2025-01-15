2fc8d43fb6b5a9050e0627b5095ff554ebb1ac20
Samuu10 <svallmun@myuax.com>
2025-01-15 11:24:48 +0100

    Commit inicial

---
+++ src/main/java/com/proyecto/nodotrabajador1/NodoTrabajador1Application.java
@@ -0,0 +1,13 @@
+package com.proyecto.nodotrabajador1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodoTrabajador1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NodoTrabajador1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/proyecto/nodotrabajador1/NodoTrabajador1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.nodotrabajador1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodoTrabajador1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
